96fa9b8da2f15c6df785a9acf934b3edde0f7997
Grzegorz Bonar <bonar111@gmail.com>
2024-10-22 07:21:51 +0200

    init

---
+++ inkflow/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to InkFlow!</h1>
+      <p>System zarządzania studiem tatuażu.</p>
+    </div>
+  );
+}
+
+export default App;
