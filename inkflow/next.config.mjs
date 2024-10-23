339661939e64ea2542da4de8808327b02fb9a992
Grzegorz Bonar <bonar111@gmail.com>
2024-10-23 18:49:05 +0200

    sad

--- inkflow/next.config.mjs
+++ inkflow/next.config.mjs
@@ -1,7 +1,15 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['book.inksearch.co'],
-    },
-  };
+  images: {
+    domains: ['book.inksearch.co'],
+  },
+  experimental: {
+    appDir: true,
+  },
+  // Dodajemy opcję basePath, jeśli to konieczne
+  // basePath: '/src',
+};
+
 export default nextConfig;
