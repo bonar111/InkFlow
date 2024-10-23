fe76c7818d67f232dc9e3ff7c6976774c7fc59b9
Grzegorz Bonar <bonar111@gmail.com>
2024-10-23 08:05:40 +0200

    asd

--- inkflow/next.config.mjs
+++ inkflow/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    images: {
+      domains: ['book.inksearch.co'],
+    },
+  };
 export default nextConfig;
